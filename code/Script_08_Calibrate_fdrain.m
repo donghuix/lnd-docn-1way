37de3636aae6f357a981843c669a8334f76bef9b
donghuix <donghui.xu@pnnl.gov>
2022-12-05 22:55:03 -0800

    modify code

--- code/Script_08_Calibrate_fdrain.m
+++ code/Script_08_Calibrate_fdrain.m
@@ -1,5 +1,7 @@
 clear;close all;clc;
 
+addpath('/Users/xudo627/donghui/mylib/m/');
+
 runs    = {'Calibration01_OCN2LND_sur_sub_coupling_22493fc.2022-11-14-131718', ...
            'Calibration02_OCN2LND_sur_sub_coupling_22493fc.2022-11-21-100919', ...
            'Calibration03_OCN2LND_sur_sub_coupling_22493fc.2022-11-17-115402', ...
@@ -33,7 +35,7 @@ end
 legend(['Fan et al' tags],'FontSize',14,'FontWeight','bold');
 
 fd_cal  = NaN(size(zwtsim,1),1);
-zwt_cal = NaN(size(zwtsim,1),1);
+%zwt_cal = NaN(size(zwtsim,1),1);
 fan8th  = -fan8th;
 for i = 1 : size(zwtsim)
     disp([num2str(i) '/' num2str(size(zwtsim,1))]);
@@ -49,13 +51,20 @@ for i = 1 : size(zwtsim)
         
         d = abs(zwtsim(i,:) - fan8th(i));
         ind = find(d == min(d));
-        zwt_cal(i) = zwtsim(i,ind(1));
+        %zwt_cal(i) = zwtsim(i,ind(1));
     else
         fd_cal(i) = 2.5;
     end
 end
 
 save('../data/Calibrated_fdrain.mat','fd_cal');
+
+load('../data/outputs/fd=cal_zwt_annual.mat','zwtyr');
+zwt_cal = nanmean(zwtyr(:,109:end),2);
+
+figure;
+plot(1:128,ones(128,1).*nanmean(fan8th),'k--','LineWidth',2); hold on; grid on;
+plot(nanmean(zwtyr,1),'r-','LineWidth',2);
 
 figure;
 plot(fan8th, zwt_cal,'b.'); hold on; grid on;
@@ -71,23 +80,46 @@ ylim([0 250]); xlim([0 250]);
 xlabel('Fan et al','FontSize',15,'FontWeight','bold');
 ylabel('Default','FontSize',15,'FontWeight','bold');
 
-[R2(1),RMSE(1),NSE(1),PBIAS(1),MSE,NSE1] = estimate_evaluation_metric(fan8th,zwt_def);
-[R2(2),RMSE(2),NSE(2),PBIAS(2),MSE,NSE1] = estimate_evaluation_metric(fan8th,zwt_cal);
+ind = find(fan8th < 800);
 
-% X = [fan8th zwt_cal];
-% figure;
-% hist3(X,'CdataMode','auto','Nbins',[100,250],'LineStyle','none');
-% view(2);colormap(jet);colorbar; ylim([0 80]); xlim([0 80]); caxis([0 100]);
-% colorbar;
+[R2(1),RMSE(1),NSE(1),PBIAS(1),MSE,NSE1] = estimate_evaluation_metric(fan8th(ind),zwt_def(ind));
+[R2(2),RMSE(2),NSE(2),PBIAS(2),MSE,NSE1] = estimate_evaluation_metric(fan8th(ind),zwt_cal(ind));
 
-% load('../data/outputs/Calibration01_zwt.mat');
-% load('../data/domain_global_coastline_merit_90m.mat');
+cmap = getPyPlot_cMap('BuPu');
+
+X = [fan8th zwt_cal];
+figure;
+hist3(X,'CdataMode','auto','Nbins',[250,80],'LineStyle','none');
+view(2);colormap(cmap);colorbar; ylim([0 80]); xlim([0 80]); caxis([0 300]);
+colorbar;
+hold on;
+h = plot3([0 80],[0 80],[2500 2500],'g--','LineWidth',2);
+xlabel('Fan et al','FontSize',15,'FontWeight','bold');
+ylabel('Calibration','FontSize',15,'FontWeight','bold');
+
+load('../data/outputs/Calibration01_zwt.mat');
+load('../data/domain_global_coastline_merit_90m.mat');
 % zwtdef = nanmean(zwt(:,100:end),2);
-% [lon,lat,zwtdef_1deg] = upscale_data(merit_x,merit_y,ones(length(zwtdef),1),zwtdef,1);
-% figure;
-% imAlpha = ones(size(zwtdef_1deg));
-% imAlpha(isnan(zwtdef_1deg)) = 0;
-% imagesc([lon(1,1),lon(end,end)],[lat(1,1),lat(end,end)],-zwtdef_1deg,'AlphaData',imAlpha); 
-% colormap(jet); colorbar; hold on; caxis([-100 0]); 
-% set(gca,'YDir','normal');
-% ylim([-60 90]);
+[lon,lat,zwtcal_1deg] = upscale_data(merit_x,merit_y,ones(length(zwt_cal),1),zwt_cal,1);
+[lon,lat,fan1deg]     = upscale_data(merit_x,merit_y,ones(length(fan8th),1),fan8th,1);
+[lon,lat,fd1deg]     = upscale_data(merit_x,merit_y,ones(length(fan8th),1),fd_cal,1);
+figure;
+imAlpha = ones(size(zwtcal_1deg));
+imAlpha(isnan(zwtcal_1deg - fan1deg)) = 0;
+imagesc([lon(1,1),lon(end,end)],[lat(1,1),lat(end,end)],zwtcal_1deg - fan1deg,'AlphaData',imAlpha); 
+colormap(blue2red(11)); colorbar; hold on; caxis([-6 6]); 
+set(gca,'YDir','normal');
+ylim([-60 90]);
+ind1 = find(zwt_cal > fan8th & (zwt_cal > 8.8 & zwt_cal < 8.88));
+plot(merit_x(ind1),merit_y(ind1),'g.'); hold on;
+ind2 = find(zwt_cal < fan8th & zwt_cal > 20);
+plot(merit_x(ind2),merit_y(ind2),'r.'); hold on;
+
+figure;
+imAlpha = ones(size(fd1deg));
+imAlpha(isnan(fd1deg)) = 0;
+imagesc([lon(1,1),lon(end,end)],[lat(1,1),lat(end,end)],fd1deg,'AlphaData',imAlpha); 
+colormap(jet); colorbar; hold on; caxis([0.1 5]); 
+set(gca,'YDir','normal');
+ylim([-60 90]);
+
